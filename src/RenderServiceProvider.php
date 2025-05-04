e7c039ced1e609d97dc1598bc0107602c6206edf
jundayw <jundayw@126.com>
2025-05-05 04:54:49 +0800

    5.x

--- src/RenderServiceProvider.php
+++ src/RenderServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Jundayw\Render;
 
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Jundayw\Render\Contracts\Renderable;
 
@@ -15,25 +14,22 @@ class RenderServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(Renderable::class, function (Application $app) {
-            return tap($app->make(RenderFactory::class), function (RenderFactory $factory) {
-                $factory->macro('success', function (?string $message = 'success', ?string $url = null, mixed $data = null) {
-                    $this->with('state', true);
-                    $this->with('message', $message);
-                    $this->with('url', $url);
-                    $this->with('data', $data);
-                    $this->with('timestamp', date('Y-m-d\TH:i:s\Z'));
-                    return $this;
-                });
-                $factory->macro('error', function (?string $error = 'error', ?string $url = null, mixed $errors = null) {
-                    $this->with('state', false);
-                    $this->with('error', $error);
-                    $this->with('url', $url);
-                    $this->with('errors', $errors);
-                    $this->with('timestamp', date('Y-m-d\TH:i:s\Z'));
-                    return $this;
-                });
-            });
+        $this->app->singleton(Renderable::class, RenderFactory::class);
+        $this->app->make(Renderable::class)->macro('success', function (?string $message = 'success', ?string $url = null, mixed $data = null) {
+            $this->with('state', true);
+            $this->with('message', $message);
+            $this->with('url', $url);
+            $this->with('data', $data);
+            $this->with('timestamp', date('Y-m-d\TH:i:s\Z'));
+            return $this;
+        });
+        $this->app->make(Renderable::class)->macro('error', function (?string $error = 'error', ?string $url = null, mixed $errors = null) {
+            $this->with('state', false);
+            $this->with('error', $error);
+            $this->with('url', $url);
+            $this->with('errors', $errors);
+            $this->with('timestamp', date('Y-m-d\TH:i:s\Z'));
+            return $this;
         });
     }
 
